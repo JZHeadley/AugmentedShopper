04d92511a5e0a2a9dea3b4d2f281bb1b7edc3e9c
parrycj <parrycj@vcu.edu>
2017-09-16 14:39:06 -0400

    layouts

---
+++ app/src/main/java/com/jzheadley/augmentedshopper/similar_items.java
@@ -0,0 +1,13 @@
+package com.jzheadley.augmentedshopper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class similar_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_similar_items);
+    }
+}
